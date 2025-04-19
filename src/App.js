4d1e3839b5463923f6e14f8602c5eb43b89cd01a
placidojr <placidojunior34@gmail.com>
2025-04-19 18:35:53 -0300

    Update App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Projects from './pages/Projects';
 import About from './pages/About';
 import Contact from './pages/Contact';
+import SetupTech from './pages/SetupTech';
 
 function App() {
   return (
@@ -11,9 +12,10 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/setuptech" element={<SetupTech />} />
       </Routes>
     </Router>
   );
